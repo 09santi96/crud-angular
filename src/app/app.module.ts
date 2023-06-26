96483f33e591bf050270771e263cc06620259d5b
09santi96 <09santio96@gmail.com>
2023-06-26 16:58:09 -0300

    routing init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+//Home - Contact - About
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes : Routes = [
+  {
+    path :'', component: HomeComponent
+  },
+  {
+    path :'Contact', component: ContactComponent
+  },
+  {
+    path :'About', component: AboutComponent
+  }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
